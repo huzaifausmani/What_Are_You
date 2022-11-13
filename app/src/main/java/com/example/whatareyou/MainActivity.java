d1650adb68c1914f9b24e1e4b2bea5e97a76c549
HuzaifaUsmani <huzaifa.1019@gmail.com>
2022-11-13 20:04:58 +0500

    Coloring and Initial Design

--- app/src/main/java/com/example/whatareyou/MainActivity.java
+++ app/src/main/java/com/example/whatareyou/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.whatareyou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String[] questions = {"1","2","3","4","5","6","7","8","9","10"},
+    answers1 = {"","","","","",""},
+    answers2 = {"","","","","",""},
+    answers3 = {"","","","","",""},
+    answers4 = {"","","","","",""},
+    answers5 = {"","","","","",""},
+    answers6 = {"","","","","",""},
+    answers7 = {"","","","","",""},
+    answers8 = {"","","","","",""},
+    answers9 = {"","","","","",""},
+    answers10 = {"","","","","",""},
+    endStatements = {"","","","","",""};
+    TextView questionText, endStatementText;
 
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
